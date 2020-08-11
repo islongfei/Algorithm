e0f01615b3c6b4a7968686c0848fad917085d062
islongfei <islongfei@163.com>
2020-08-11 23:50:08 +0800

    add leetcode 144

---
+++ src/com/longfei/medium/PreorderTraversal144.java
@@ -0,0 +1,42 @@
+package com.longfei.medium;
+
+import javax.swing.tree.TreeNode;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 二叉树的前序遍历
+ */
+public class PreorderTraversal144 {
+
+    // 使用一个辅助栈，每当走一次 中->左->右， 就会从栈中移除这个节点（中）
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack();
+        TreeNode cur = root;
+
+        while (cur != null || !stack.isEmpty()) {
+            while (cur != null) {
+                stack.push(cur);
+                list.add(cur.val);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            cur = cur.right;
+        }
+
+        return list;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+}
