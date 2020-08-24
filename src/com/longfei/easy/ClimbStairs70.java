3ed10b0d7f3be788437b3079c7216bffb505f7c7
islongfei <islongfei@163.com>
2020-08-24 22:39:00 +0800

    add leetcode 11

--- src/com/longfei/easy/ClimbStairs70.java
+++ src/com/longfei/easy/ClimbStairs70.java
@@ -6,6 +6,7 @@ package com.longfei.easy;
 public class ClimbStairs70 {
 
     /**
+     * 化繁为简：
      * 因为每次只能走 1-2 个台阶，那么走到第 n 个台阶时一定是 n-1 或者 n-2 个台阶上来的。
      * 第3阶的走法 = 第1阶的走法 + 第二阶的走法
      * 归纳为 f(n) = f(n-1) + f(n-2) 且不会有重复的走法
