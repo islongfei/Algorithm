15b83a6a4779e5c6a6b1c23db4f51ef746c0ec59
islongfei <islongfei@163.com>
2020-08-28 00:01:09 +0800

    add leetcode 24

---
+++ src/com/longfei/medium/SwapPairs24.java
@@ -0,0 +1,33 @@
+package com.longfei.medium;
+
+/**
+ * 两两交换链表中的节点
+ */
+public class SwapPairs24 {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode swapPairs(ListNode head) {
+        //存放新组成的链表
+        ListNode pre = new ListNode(0);
+        pre.next = head;
+        ListNode temp = pre;
+        while (temp.next != null && temp.next.next != null) {
+            ListNode start = temp.next;
+            ListNode end  = temp.next.next;
+            temp.next = end;
+            start.next = end.next;
+            end.next = start;
+            temp = start;
+        }
+        return pre.next;
+    }
+
+}
