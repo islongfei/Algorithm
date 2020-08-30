bae424fcf5453a7a25b6217ad836c9f1cae91502
islongfei <islongfei@163.com>
2020-08-30 15:51:47 +0800

    update leetcode 11

--- src/com/longfei/medium/MaxArea11.java
+++ src/com/longfei/medium/MaxArea11.java
@@ -4,7 +4,7 @@ package com.longfei.medium;
  * 盛水最多的容器
  */
 public class MaxArea11 {
-    //双指针法
+    //双指针法（左右夹逼）
     //向内移动一格板，拿到前后的最大面积
     //向内移动短板，面积有可能增大，向内移动长板，面积一定会减少
     public int maxArea(int[] height) {
