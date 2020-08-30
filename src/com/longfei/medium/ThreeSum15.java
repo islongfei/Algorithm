ec422237d974053bb9cfc65b1fa88825658c15e3
islongfei <islongfei@163.com>
2020-08-30 21:20:42 +0800

    add leetcode 15

---
+++ src/com/longfei/medium/ThreeSum15.java
@@ -0,0 +1,25 @@
+package com.longfei.medium;
+
+import java.util.List;
+
+/**
+ * 三数之和
+ *
+ * @author islongfei
+ * @date 2020.08.30
+ */
+public class ThreeSum15 {
+
+    /**
+     * 三数之和 = 0
+     * <p>
+     * solution 1. 三重暴力循环
+     * solution 2. 两重暴力 + hash （类似两数之和，转化为 a + b = -c）
+     * solution 3. 因为不需要下班，双指针左右夹逼
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        //第三个题解
+        return null;
+    }
+
+}
