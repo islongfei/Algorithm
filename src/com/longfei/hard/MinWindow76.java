2fae46fb612d17830cb9746f9193321cfbc27a15
wanglongfei <islongfei@163.com>
2021-01-20 14:21:13 +0800

    add num76

---
+++ src/com/longfei/hard/MinWindow76.java
@@ -0,0 +1,21 @@
+package com.longfei.hard;
+
+import java.util.HashMap;
+
+/**
+ * 最小覆盖字串
+ *
+ * @author islongfei
+ * @date 2021.01.18
+ */
+public class MinWindow76 {
+    public String minWindow(String s, String t) {
+     // s = "ADOBECODEBANC", t = "ABC"
+        HashMap<Character, Integer> map = new HashMap<>();
+        int left = 0;
+         for (int i = 0; i < s.length(); i++) {
+         
+         }
+        return null;
+    }
+}
