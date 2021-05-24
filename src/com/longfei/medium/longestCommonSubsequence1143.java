9fe492edde404e369c20fe1cfdce6e107e0d3f17
islongfei <islongfei@163.com>
2021-05-25 00:25:56 +0800

    add leetcode 1143

---
+++ src/com/longfei/medium/longestCommonSubsequence1143.java
@@ -0,0 +1,23 @@
+package com.longfei.medium;
+
+/**
+ * @author islongfei
+ * @date 2021.05.25
+ * 求两个字符串的最长 公共子序列 的长度
+ */
+public class longestCommonSubsequence1143 {
+
+    // "abcde" "ace" ->3
+    public int longestCommonSubsequence(String text1, String text2) {
+        return 0;
+    }
+
+    /**
+     *  1、 暴力：生成完所有子序列，看在另一个是否存在
+     *  2、 DP: 转换子问题：
+     *      用二维数组表示两个字符串，
+     *
+     *
+     */
+
+}
