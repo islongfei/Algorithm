b21b056a2a30308e0e5669d9fbdf375af5171cd8
wanglongfei <islongfei@163.com>
2021-06-01 18:17:38 +0800

    update hot 46

--- src/com/longfei/medium/PermuteHot46.java
+++ src/com/longfei/medium/PermuteHot46.java
@@ -9,8 +9,20 @@ import java.util.List;
  * 全排列（回溯）
  */
 public class PermuteHot46 {
+    // 回溯模板
+    // result = []
+    // def backtrack(路径, 选择列表):
+    //     if 满足结束条件:
+    //     result.add(路径)
+    //     return
+    //
+    //     for 选择 in 选择列表:
+    // 做选择
+    // backtrack(路径, 选择列表)
+    // 撤销选择
     
     List<List<Integer>> res = new LinkedList<>();
+    
     // https://mp.weixin.qq.com/s/nMUHqvwzG2LmWA9jMIHwQQ
     public List<List<Integer>> permute1(int[] nums) {
         LinkedList<Integer> track = new LinkedList<>();
