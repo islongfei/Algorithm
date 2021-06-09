83b904e011557d3b40cd86e3e49b0a4b108bc5be
wanglongfei <islongfei@163.com>
2021-06-09 16:47:19 +0800

    update note

--- src/com/longfei/easy/RemoveDuplicates26.java
+++ src/com/longfei/easy/RemoveDuplicates26.java
@@ -9,10 +9,9 @@ public class RemoveDuplicates26 {
         //动画视频：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/solution/shi-ping-dong-hua-jie-xi-bao-ni-dong-by-novice2mas/
         if (nums == null || nums.length == 0) return 0;
         int i = 0, j = 1;//i记录不重复的元素
-
         while (j < nums.length) {
             if (nums[i] != nums[j]) {
-                nums[++i] = nums[j];
+                nums[++i] = nums[j]; //和上一个数不相等直接赋值，前i个都是不重复的元素
             }
             j++;
         }
